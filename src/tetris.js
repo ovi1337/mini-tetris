89df69ecb81b9e561caedd3ff9c4f313da5c1657
Becky <becky@monometric.net>
2017-10-14 00:58:37 +0200

    550 implement bag of seven

--- src/tetris.js
+++ src/tetris.js
@@ -1,6 +1,8 @@
 // _() – Main loop
 // M(code) – Map P to p using code and merge it with field B into field b.
 //           Return true if piece overlaps with B.
+// o(index) – Pick the next shape from the current bag recursively.
+//            Create bag if needed.
 
 // B – Current state of playing field, format: [c00<<0|c01<<1|…, c10<<0|c11<<1|…, …]
 // b - Updated state of playing field
@@ -10,6 +12,7 @@
 // k – Last key code pressed (- 38)
 // S - Total score
 // s – Tally of current move used to reward clearing multiple lines, keyboard event
+// O – Bag of seven
 
 M=e=>P&&(
     h=0,
@@ -24,6 +27,7 @@ M=e=>P&&(
     h
 ),
 b=C=Array(9),
+o=e=>(O=O||127)>>e%7&1?(O^=1<<e%7,e):o(e+1),
 (onkeydown=_=s=>(
     s
         ?(k=s.which-38)%2-k||M`k?v-!E*k:P[2]-(P[i^1]-P[3])*(E|1)`
@@ -31,7 +35,7 @@ b=C=Array(9),
             :P=p
         :(
             P=P||[...'02121303040506161715'.substr(
-                new Date%7*2,
+                o(new Date%7)%7*2,
                 8,
                 B=[...C,...b].filter(v=>v^1023||!(S+=++s)).slice(-18)
             )],
@@ -41,4 +45,4 @@ b=C=Array(9),
         ),
         S|=b.map(v=>{S+=`
 `;for(x=10;x--;)Z.innerText=S+='□■'[v^1023+P&&v>>x&1]})
-))(S=P=k=0)
+))(S=P=k=O=0)
