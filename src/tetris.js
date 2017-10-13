3737a5db4c5917ceed4dbc93b096bbe22ce656a2
Becky <becky@monometric.net>
2017-10-14 01:02:26 +0200

    543

--- src/tetris.js
+++ src/tetris.js
@@ -28,21 +28,21 @@ M=e=>P&&(
 ),
 b=C=Array(9),
 o=e=>(O=O||127)>>e%7&1?(O^=1<<e%7,e):o(e+1),
-(onkeydown=_=s=>(
+(onkeyup=_=s=>(
     s
         ?(k=s.which-38)%2-k||M`k?v-!E*k:P[2]-(P[i^1]-P[3])*(E|1)`
             ?M`v`
             :P=p
         :(
             P=P||[...'02121303040506161715'.substr(
-                o(new Date%7)%7*2,
+                o(I)%7*2,
                 8,
                 B=[...C,...b].filter(v=>v^1023||!(S+=++s)).slice(-18)
             )],
             M`v-E`&&M`v`&(p=k=0),
-            setTimeout(_,k-2&&200,0),
+            I=setTimeout(_,k-2&&200,0),
             P=p
         ),
         S|=b.map(v=>{S+=`
 `;for(x=10;x--;)Z.innerText=S+='□■'[v^1023+P&&v>>x&1]})
-))(S=P=k=O=0)
+))(S=P=k=O=I=0)
